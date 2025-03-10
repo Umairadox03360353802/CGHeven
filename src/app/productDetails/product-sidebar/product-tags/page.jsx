816b337719d966d88fa417b2060f17546cfb7504
Umair Mahmood <uMehmood@skillgigs.com>
2025-03-10 07:13:24 +0500

    all fixes

--- src/app/productDetails/product-sidebar/product-tags/page.jsx
+++ src/app/productDetails/product-sidebar/product-tags/page.jsx
@@ -3,20 +3,21 @@
 
 import { Badge } from "@/components/ui/badge"
 
-/**
- * Tags component for displaying tags
- * @param {Object} props
- * @param {string[]} [props.tags] - Tags to display
- */
+// /**
+//  * Tags component for displaying tags
+//  * @param {Object} props
+//  * @param {any} [props.tags] - Tags to display
+//  */
 
-export default function ProductTags({ tags = ["sun", "mountain", "view", "hilltop", "dam"] }) {
 
- 
+export default function ProductTags(  ) {
+
+ const Ptags = ["sun", "mountain", "view", "hilltop", "dam"]
 
   return (
     <div className="flex flex-wrap items-center gap-2 p-2 bg-gray-800 ">
     <span className="text-white mr-1">Tags:</span>
-    {tags.map((tag) => (
+    {Ptags.map((tag) => (
       <Badge key={tag} className="bg-teal-600 hover:bg-teal-500 text-white text-sm rounded border-none">
         {tag}
       </Badge>
